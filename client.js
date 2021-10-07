fa04f70d74da5d6de9ce2ead6b7935ee69c0a641
LaScheggia <lascheggia605@gmail.com>
2021-10-07 19:40:11 +0200

    finishing indent

--- client.js
+++ client.js
@@ -3,24 +3,24 @@ const net = require("net");
 
 const command_success = require("./command_success.json");
 
-
 //Selecting the port connection
 const options = {
   port: 8080,
 };
 
-
 //Connection
 let client = net.connect(options, () => {
   client.on('data', data =>{
-    console.log(data.toString("utf8"));
+
+    const commandRes = JSON.parse(data.toString("utf8"));
+    const sumRes = commandRes.examples[0].event.payload.result;
+
+    console.log(sumRes);
     client.destroy();
   })
   console.log("connected!");
   
   //Sending json stringified to server
   client.write(JSON.stringify(command_success));
-
-  
 });
 
