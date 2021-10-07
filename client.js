bf6872e74e5e7ea18965a1c04879c45b61408fc0
LaScheggia <lascheggia605@gmail.com>
2021-10-07 19:35:04 +0200

    send and receives something

--- client.js
+++ client.js
@@ -1,15 +1,26 @@
 // Including Nodejs' net module.
 const net = require("net"); 
 
+const command_success = require("./command_success.json");
+
+
 //Selecting the port connection
 const options = {
   port: 8080,
-
 };
+
 
 //Connection
 let client = net.connect(options, () => {
-  client.write("sir are u there?");
+  client.on('data', data =>{
+    console.log(data.toString("utf8"));
+    client.destroy();
+  })
   console.log("connected!");
+  
+  //Sending json stringified to server
+  client.write(JSON.stringify(command_success));
+
+  
 });
 
--- server.js
+++ server.js
@@ -4,11 +4,28 @@ const net = require("net");
 //This is server's listening port
 const port = 8080;
 
+//Requesting event.json
+const event = require("./event.json");
+
+//Creating a func to sum
+function sumNumbers(x, y){
+  return x+y;
+}
+
 //Creating server with net.createServer
 let server = net.createServer(socket => {
   socket.on('data', data => {
-    // do something with data
-    console.log(data.toString("utf8"));
+
+    const command = JSON.parse(data.toString("utf8"));
+    const payload = command.examples[0].command.payload;
+    const sumResult = sumNumbers(payload.x, payload.y);
+    event.examples[0].event.payload.result = sumResult;
+
+    socket.write(JSON.stringify(event));
+    //console.log(command);
+    //console.log(payload);
+    //console.log(sumResult);
+    //console.log(JSON.stringify(event));
   });
 });
 
